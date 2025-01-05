ffa2549e32a6b0527266cbf0adadbb80c270ce6f
long.nh <longnh.uit@gmail.com>
2025-01-05 10:44:06 +0700

    feat: student detail page UI

--- src/lib/graphql/student.action.ts
+++ src/lib/graphql/student.action.ts
@@ -11,12 +11,12 @@ const GET_ALL_STUDENTS = gql`
 `;
 
 const GET_STUDENT_BYID = gql`
-query findOneStudent($id: String!){
-    findOneStudent (id: $id){
-        id
-        classId
-        studentName
+  query findOneStudent($id: String!) {
+    findOneStudent(id: $id) {
+      id
+      classId
+      studentName
     }
-}
-`
+  }
+`;
 export { GET_ALL_STUDENTS, GET_STUDENT_BYID };
---
+++ src/pages/students/[id].tsx
@@ -0,0 +1,97 @@
+import client from '@/lib/graphql/apolloClient';
+import { GET_ALL_CLASSES, GET_CLASS_BYID } from '@/lib/graphql/Classes.action';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import * as React from 'react';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
+import { GET_ALL_STUDENTS, GET_STUDENT_BYID } from '@/lib/graphql/student.action';
+import Link from 'next/link';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query({
+    query: GET_ALL_STUDENTS,
+  });
+
+  const paths = data.findAllStudent.map((studentItem: { id: string }) => ({
+    params: { id: studentItem.id },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { id } = context.params!;
+  const { data } = await client.query({
+    query: GET_STUDENT_BYID,
+    variables: { id },
+  });
+
+  return {
+    props: {
+      studentData: data.findOneStudent,
+    },
+  };
+};
+
+const StudentPageDetail = ({ studentData }: { studentData: { studentName: string; classId: string; id: string } }) => {
+  console.log(studentData);
+  return (
+    <div className="flex justify-center">
+      <div className="w-[900px] bg-white min-h-screen rounded-sm pt-5 pl-5 pr-5 ">
+        <div className="flex justify-center">
+          <Card className="w-[550px]">
+            <CardHeader className="items-center">
+              <CardTitle>STUDENT INFORMATION DETAIL</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Student ID</Label>
+                    <div className="flex flex-row">
+                      <Input id="classId" disabled placeholder={studentData.id} />
+                      <Button>
+                        <Copy />
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="className">Student Name</Label>
+                    <div className="flex flex-row">
+                      <Input id="className" disabled placeholder={studentData.studentName} className="text-black" />
+                      <Button>
+                        <Copy />
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="className">Class Name</Label>
+                    <div className="flex flex-row">
+                      <Input id="className" disabled placeholder={studentData.classId} className="text-black" />
+                      <Button>
+                        <Copy />
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline">
+                <Link href="/students">Back</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentPageDetail;
