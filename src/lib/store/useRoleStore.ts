435aec4b199e87edbb2f71ba6de9e03cda67fa08
longnh462 <longnh.uit@gmail.com>
2025-02-05 19:13:58 +0700

    add store role

---
+++ src/lib/store/useRoleStore.ts
@@ -0,0 +1,5 @@
+const useRoles = () => {
+    
+}
+
+export default useRoles;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="w-[900px] bg-white min-h-screen rounded-sm pt-5 pl-5">
-        <div className="flex justify-center py-10" >
+        <div className="flex flex-col items-center justify-center py-10" >
           <RolesSelectionCombobox/>
-          <span>
+          <span className="text-black">
             <p>Your are logged as {}</p>
           </span>
         </div>
